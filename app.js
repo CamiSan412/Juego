29cfc9d1fd5ba2c3d614e4c48c62c3f1e769d280
CamiSan412 <camilasanchezamaya01@gmail.com>
2025-03-08 19:26:40 -0500

    Actualización del proyecto

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let numeroSecreto = 0
 let intentos = 0
 let listaNumerosSorteados = []
@@ -83,3 +84,90 @@ condicionesIniciales()
 
 
 
+=======
+let numeroSecreto = 0
+let intentos = 0
+let listaNumerosSorteados = []
+let numeroMaximo = 10
+
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento)
+    elementoHTML.innerHTML= texto 
+    return
+} 
+
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value) 
+    
+    if( numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p', `Acertaste el numero en ${intentos} ${(intentos === 1) ? 'vez' : 'veces' }`)
+        document.getElementById('reiniciar').removeAttribute('disabled')
+    }else{
+        //ususario no acerto
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El numero es Menor')
+        }else{
+            asignarTextoElemento('p', 'El numero es Mayor')
+        }
+        intentos++
+        limpiarCaja()
+    }    
+    return
+}
+
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = ''
+}
+
+
+function generarNumeroSecreto(){
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1
+
+    console.log(numeroGenerado)
+    console.log(listaNumerosSorteados)
+
+    //Si ya sorteamos todosa los numeros 
+    if(listaNumerosSorteados.length === numeroMaximo){
+        asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles')
+    }else{
+        //si el numero generado esta incluido en la lista
+        if(listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto()
+        }else{
+            listaNumerosSorteados.push(numeroGenerado)
+            return numeroGenerado
+        }
+    }   
+    
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del numero secreto');
+    asignarTextoElemento('p', `Indica un numero del 1 al ${numeroMaximo}.`);
+    numeroSecreto = generarNumeroSecreto()
+    intentos = 1
+}
+
+function reiniciarJuego(){
+    //Limpiar caja
+    limpiarCaja()
+
+    //Indicar mensaje de intervalo de numeros, Generar el numero aleatorio, Inicilizar el numero de intentos
+    condicionesIniciales()       
+
+    //Deshabilitar el boton de Nuevo Juego   
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true') 
+}
+
+condicionesIniciales()
+
+
+
+
+
+
+
+>>>>>>> b4b76df (Actualización del proyecto)
--- ejercicio1.js
+++ ejercicio1.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function mensaje(){
     console.log('Hola mundo')
     return
@@ -90,4 +91,98 @@ function cuadrado(numero) {
 }
   
 let resultado = cuadrado(2);
+=======
+function mensaje(){
+    console.log('Hola mundo')
+    return
+}
+
+mensaje() 
+
+/* function  mensajePersonalizado(nombre){
+    nombre = 'camila'
+    console.log(`Hola ${nombre}`) 
+    return
+}
+
+mensajePersonalizado() */
+
+function mostrarHolaNombre(nombre) {
+    console.log(`¡Hola, ${nombre}!`);
+  }
+  
+mostrarHolaNombre("Alice");
+
+
+
+/* function  dobleDeNumero(numero){
+    numero = 14
+    console.log(parseInt(numero * 2)) 
+    return
+}
+
+dobleDeNumero()
+ */
+
+function calcularDoble(numero) {
+    return numero * 2;
+  }
+  
+let resultadoDoble = calcularDoble(5);
+console.log(resultadoDoble);
+
+
+/* function  promedio(valor1, valor2, valor3){
+    valor1 = 20
+    valor2= 30
+    valor3 = 40
+    console.log((valor1 + valor2 + valor3) / 4) 
+    return
+}
+
+promedio() */
+
+function calcularPromedio(a, b, c) {
+    return (a + b + c) / 3;
+  }
+  
+let promedio = calcularPromedio(4, 7, 10);
+console.log(promedio);
+
+/* function  numeroMayor(num1, num2){
+    num1 = 10
+    num2 = 25
+    if(num1 > num2){
+        console.log(`El numero mayor es ${num1}`)
+    }else{
+        console.log(`El numero mayor es ${num2}`)
+    }
+    console.log() 
+    return
+} 
+numeroMayor()
+*/
+
+
+function encontrarMayor(a, b) {
+    return a > b ? a : b;
+}  
+let numeroMayor = encontrarMayor(15, 9);
+console.log(numeroMayor);
+
+
+/* function numeroMultiplicado (numero){
+    numero = 2
+    console.log(numero * numero) 
+    return
+}
+
+numeroMultiplicado()  */
+
+function cuadrado(numero) {
+    return numero * numero;
+}
+  
+let resultado = cuadrado(2);
+>>>>>>> b4b76df (Actualización del proyecto)
 console.log(resultado);
